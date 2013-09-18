e517ae802600109e9414491f65417bb814fe84c2
tianrenw <twang@robotex.com>
2013-09-17 17:41:17 -0700

    2nd commit

---
+++ src/aa.cpp
@@ -0,0 +1,18 @@
+/*
+ * aa.cpp
+ *
+ *  Created on: Sep 17, 2013
+ *      Author: TWang
+ */
+
+#include "aa.h"
+
+aa::aa() {
+	// TODO Auto-generated constructor stub
+
+}
+
+aa::~aa() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/aa.h
@@ -0,0 +1,17 @@
+/*
+ * aa.h
+ *
+ *  Created on: Sep 17, 2013
+ *      Author: TWang
+ */
+
+#ifndef AA_H_
+#define AA_H_
+
+class aa {
+public:
+	aa();
+	virtual ~aa();
+};
+
+#endif /* AA_H_ */
